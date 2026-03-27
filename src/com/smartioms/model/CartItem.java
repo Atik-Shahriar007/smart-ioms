89111f510f13eba69a3e72c98792411d304c66b3
asif-azadz <asifalazad@iut-dhaka.edu>
2026-03-28 04:36:09 +0600

    shopping cart item model
    - stores product references 
    - provides getter/setter for quantity ( used by cartservice , updatequantity
    - get total price ()- SELLING PRC* Quantity
    - used by cartservice for cart opration & cstmerpnel for disp

---
+++ src/com/smartioms/model/CartItem.java
@@ -0,0 +1,10 @@
+package com.smartioms.model;
+public class CartItem {
+    private Product product;
+    private int quantity;
+    public CartItem(Product product, int quantity) { this.product = product; this.quantity = quantity; }
+    public Product getProduct() { return product; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int q) { this.quantity = q; }
+    public double getTotalPrice() { return product.getSellingPrice() * quantity; }
+}
