251c24220f1d2302005f3adc7e1a59e114e88862
asif-azadz <asifalazad@iut-dhaka.edu>
2026-03-28 05:30:51 +0600

    order item model
    - similar to cartitem but immutable (no setquantity method)
    - stores product reference and ordered quantity
    - created during checkout from cartitemdata
    - used by invoice manager & invoice for price calculation

---
+++ src/com/smartioms/model/OrderItem.java
@@ -0,0 +1,13 @@
+package com.smartioms.model;
+
+public class OrderItem {
+    private Product product;
+    private int quantity;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+    public Product getProduct() { return product; }
+    public int getQuantity() { return quantity; }
+}
